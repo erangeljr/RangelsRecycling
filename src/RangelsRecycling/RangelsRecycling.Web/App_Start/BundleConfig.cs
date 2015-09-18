84dcd286a72223373d3ae76dadc34f513b8fac0c
erangeljr <hacker@eddierangel.com>
2015-09-17 22:53:34 -0700

    Adding CSS

--- src/RangelsRecycling/RangelsRecycling.Web/App_Start/BundleConfig.cs
+++ src/RangelsRecycling/RangelsRecycling.Web/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace RangelsRecycling.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/main.css"));
         }
     }
 }
