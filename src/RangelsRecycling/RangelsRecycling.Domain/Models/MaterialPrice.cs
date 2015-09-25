59961b8c99ccfa650e4f8d39706a972518cfa696
Eddie Rangel <eddie.rangel@doverals.com>
2015-09-25 12:18:14 -0700

    Adding Domain Models. Adding View Models.

---
+++ src/RangelsRecycling/RangelsRecycling.Domain/Models/MaterialPrice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangelsRecycling.Domain.Models
+{
+    public class MaterialPrice
+    {
+        public int MaterialPriceId { get; set; }
+        public decimal CrvRate { get; set; }
+        public decimal ScrapRate { get; set; }
+        public DateTime MaterialPriceDate { get; set; }
+    }
+}
