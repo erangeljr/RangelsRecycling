0743595406e4eceb85b2e22d4e54fd935560589f
Eddie Rangel <eddie.rangel@doverals.com>
2015-09-30 14:48:08 -0700

    Adding Models

---
+++ src/RangelsRecycling/RangelsRecycling.Domain/Models/Facility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangelsRecycling.Domain.Models
+{
+    public class Facility
+    {
+        public int FacilityId { get; set; }
+        public string Name { get; set; }
+        public string CertificationId { get; set; }
+
+    }
+}
